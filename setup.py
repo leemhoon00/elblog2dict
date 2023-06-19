0a916ecaac5d70795fb907b8e770c2ffabc96ee0
leemhoon00 <leemhoon00@naver.com>
2023-06-19 19:21:25 +0900

    release: 1.0.0

--- setup.py
+++ setup.py
@@ -1,15 +1,18 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text(encoding='utf-8')
 
 setup(
     name='elblog2dict',
-    version='0.0.3',
-    description='temp',
+    version='1.0.0',
+    description='Utility for parsing and extracting data from ELB logs',
     author='leemhoon00',
     author_email='leemhoon000@gmail.com',
-    url='',
+    url='https://github.com/leemhoon00/elblog2dict',
     install_requires=[],
     packages=find_packages(exclude=['.git', '.gitignore', '*.gz', 'test.py']),
-    keywords=['elb'],
+    keywords=['elb', 'log', 'parser', 'elblog2dict', 'regular expression'],
     python_requires='>=3.6',
     package_data={},
     zip_safe=False,
@@ -19,5 +22,7 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
     ],
-    license='MIT'
+    license='MIT',
+    long_description=long_description,
+    long_description_content_type='text/markdown'
 )
