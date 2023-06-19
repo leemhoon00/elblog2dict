9a9abed3ec07c31eed17b1a3d4cd00c93c74b76a
leemhoon00 <leemhoon00@naver.com>
2023-06-19 17:10:21 +0900

    initial commit

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='elblog2dict',
+    version='0.0.1',
+    description='temp',
+    author='leemhoon00',
+    author_email='leemhoon000@gmail.com',
+    url='',
+    install_requires=[],
+    packages=find_packages(exclude=['.git', '.gitignore', '*.gz', 'test.py']),
+    keywords=['elb'],
+    python_requires='>=3.6',
+    package_data={},
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    license='MIT'
+)
