6ad29cd9134851757dc097bada96f55b2c256dcf
leemhoon00 <leemhoon00@naver.com>
2023-06-19 18:04:16 +0900

    0.0.3

--- elblog2dict/parser.py
+++ elblog2dict/parser.py
@@ -1,12 +1,13 @@
 import re
 
-fields = ['type', 'timestamp', 'elb', 'client_ip', 'clent_port', 'target_ip', 'target_port', 'request_processing_time', 'target_processing_time',
+def parse(datas):
+    
+    fields = ['type', 'timestamp', 'elb', 'client_ip', 'clent_port', 'target_ip', 'target_port', 'request_processing_time', 'target_processing_time',
           'response_processing_time', 'elb_status_code', 'target_status_code', 'received_bytes', 'sent_bytes',
           'request_method', 'url', 'http_version', 'user_agent', 'ssl_cipher', 'ssl_protocol', 'target_group_arn', 'trace_id', 'domain_name', 
           'chosen_cert_arn', 'matched_rule_priority', 'request_creation_time', 'actions_executed', 'redirect_url', 'error_reason', 
           'target_port_list', 'target_status_code_list', 'classification', 'classification_reason']
-
-def parse(datas):
+    
     result = []
     if type(datas) == bytes:
         datas = datas.decode('utf-8')
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='elblog2dict',
-    version='0.0.2',
+    version='0.0.3',
     description='temp',
     author='leemhoon00',
     author_email='leemhoon000@gmail.com',
